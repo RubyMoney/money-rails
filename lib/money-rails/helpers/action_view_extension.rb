2eecada82f57a5882fe5537303dab14053110083
Andreas Loupasakis <andreas@aloop.org>
2012-06-19 21:30:55 +0300

    Helpers for humanizing money values and objects

--- lib/money-rails/helpers/action_view_extension.rb
+++ lib/money-rails/helpers/action_view_extension.rb
@@ -4,5 +4,33 @@ module MoneyRails
     def currency_symbol
       content_tag(:span, Money.default_currency.symbol, :class => "currency_symbol")
     end
+
+    def humanized_money(value, symbol=false)
+      if value.is_a?(Money)
+        value.format(no_cents_if_whole: true, symbol: symbol)
+      elsif value.respond_to?(:to_money)
+        value.to_money.format(no_cents_if_whole: true, symbol: symbol)
+      else
+        ""
+      end
+    end
+
+    def humanized_money_with_symbol(value)
+      humanized_money(value, true)
+    end
+
+    def money_without_cents(value, symbol=false)
+      if value.is_a?(Money)
+        value.format(no_cents: true, symbol: symbol)
+      elsif value.respond_to?(:to_money)
+        value.to_money.format(no_cents: true, symbol: symbol)
+      else
+        ""
+      end
+    end
+
+    def money_without_cents_and_with_symbol(value)
+      money_without_cents(value, true)
+    end
   end
 end
--- spec/helpers/action_view_extension_spec.rb
+++ spec/helpers/action_view_extension_spec.rb
@@ -6,4 +6,33 @@ describe 'MoneyRails::ActionViewExtension' do
     it { should be_a String }
     it { should include Money.default_currency.symbol }
   end
+
+  describe '#humanized_money' do
+    subject { helper.humanized_money Money.new(12500) }
+    it { should be_a String }
+    it { should_not include Money.default_currency.symbol }
+    it { should_not include Money.default_currency.decimal_mark }
+  end
+
+  describe '#humanized_money_with_symbol' do
+    subject { helper.humanized_money_with_symbol Money.new(12500) }
+    it { should be_a String }
+    it { should_not include Money.default_currency.decimal_mark }
+    it { should include Money.default_currency.symbol }
+  end
+
+  describe '#money_without_cents' do
+    subject { helper.money_without_cents Money.new(12500) }
+    it { should be_a String }
+    it { should_not include Money.default_currency.symbol }
+    it { should_not include Money.default_currency.decimal_mark }
+  end
+
+  describe '#money_without_cents_and_with_symbol' do
+    subject { helper.money_without_cents_and_with_symbol Money.new(12500) }
+    it { should be_a String }
+    it { should_not include Money.default_currency.decimal_mark }
+    it { should include Money.default_currency.symbol }
+  end
+
 end
