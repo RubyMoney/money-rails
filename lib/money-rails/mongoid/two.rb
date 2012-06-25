1bd9673400ec3c084c0c65fcd3cf7a35b1999794
Kevin Sołtysiak <soltysiak.kevin@gmail.com>
2012-06-25 15:16:16 +0200

    Serialization of Money objects for Mongoid 2.X

---
+++ lib/money-rails/mongoid/two.rb
@@ -0,0 +1,25 @@
+# Class name does not really matches the folder hierarchy, because
+# in order for (de)serialization to work, the class must be re-opened.
+# But this file brings mongoid 2.X compat., so...
+
+class Money
+  include ::Mongoid::Fields::Serializable
+
+  # Mongo friendly -> Money
+  def deserialize(object)
+    return nil if object.nil?
+
+    object = object.with_indifferent_access
+    ::Money.new object[:cents], object[:currency_iso]
+  end
+
+  # Money -> Mongo friendly
+  def serialize(object)
+    return nil unless object.is_a? Money
+
+    {
+      :cents        => object.cents,
+      :currency_iso => object.currency.iso_code
+    }
+  end
+end
