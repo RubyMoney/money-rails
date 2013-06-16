3a19b1795f96dc696d5ab160f8bbed125016797a
Andreas Loupasakis <alup@aloop.org>
2013-06-17 01:08:15 +0300

    Let us use focus on specific tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,4 +16,5 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
