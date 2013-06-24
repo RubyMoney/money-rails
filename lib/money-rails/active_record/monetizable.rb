66e9862272c62c768941b92ce074eca3a4223f08
Borna Novak <dosadnizub@gmail.com>
2013-06-24 12:14:11 +0200

    Fix for #112, possibly

--- lib/money-rails/active_record/monetizable.rb
+++ lib/money-rails/active_record/monetizable.rb
@@ -153,7 +153,8 @@ module MoneyRails
           end
 
           define_method "currency_for_#{name}" do
-            if self.respond_to?(instance_currency_name) && send(instance_currency_name).present? 
+            if self.respond_to?(instance_currency_name) && send(instance_currency_name).present? &&
+                Money::Currency.find(send(instance_currency_name))
               Money::Currency.find(send(instance_currency_name))
             elsif field_currency_name
               Money::Currency.find(field_currency_name)
