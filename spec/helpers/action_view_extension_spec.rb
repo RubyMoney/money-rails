54ec426c0c921cae4226cae3373b2994421cfcbe
Jamie Dyer <jamie@kernowsoul.com>
2014-05-23 12:19:09 +0100

    gh-157 helpers respect no_cents_if_whole configuration option

--- spec/helpers/action_view_extension_spec.rb
+++ spec/helpers/action_view_extension_spec.rb
@@ -56,6 +56,86 @@ describe 'MoneyRails::ActionViewExtension' do
     it { should be_a String }
     it { should_not include Money.default_currency.decimal_mark }
     it { should include Money.default_currency.symbol }
+    it { should_not include "00" }
+  end
+
+  context 'respects MoneyRails::Configuration settings' do
+    context 'with no_cents_if_whole: false' do
+
+      before do
+        MoneyRails.configure do |config|
+          config.no_cents_if_whole = false
+        end
+      end
+
+      describe '#humanized_money' do
+        subject { helper.humanized_money Money.new(12500) }
+        it { should be_a String }
+        it { should_not include Money.default_currency.decimal_mark }
+        it { should_not include Money.default_currency.symbol }
+        it { should include "00" }
+      end
+
+      describe '#humanized_money_with_symbol' do
+        subject { helper.humanized_money_with_symbol Money.new(12500) }
+        it { should be_a String }
+        it { should_not include Money.default_currency.decimal_mark }
+        it { should include Money.default_currency.symbol }
+        it { should include "00" }
+      end
+    end
+
+    context 'with no_cents_if_whole: nil' do
+
+      before do
+        MoneyRails.configure do |config|
+          config.no_cents_if_whole = nil
+        end
+      end
+
+      describe '#humanized_money' do
+        subject { helper.humanized_money Money.new(12500) }
+        it { should be_a String }
+        it { should_not include Money.default_currency.decimal_mark }
+        it { should_not include Money.default_currency.symbol }
+        it { should_not include "00" }
+      end
+
+      describe '#humanized_money_with_symbol' do
+        subject { helper.humanized_money_with_symbol Money.new(12500) }
+        it { should be_a String }
+        it { should_not include Money.default_currency.decimal_mark }
+        it { should include Money.default_currency.symbol }
+        it { should_not include "00" }
+      end
+    end
+
+    context 'with no_cents_if_whole: true' do
+
+      before do
+        MoneyRails.configure do |config|
+          config.no_cents_if_whole = true
+        end
+      end
+
+      describe '#humanized_money' do
+        subject { helper.humanized_money Money.new(12500) }
+        it { should be_a String }
+        it { should_not include Money.default_currency.decimal_mark }
+        it { should_not include Money.default_currency.symbol }
+        it { should_not include "00" }
+      end
+
+      describe '#humanized_money_with_symbol' do
+        subject { helper.humanized_money_with_symbol Money.new(12500) }
+        it { should be_a String }
+        it { should_not include Money.default_currency.decimal_mark }
+        it { should include Money.default_currency.symbol }
+        it { should_not include "00" }
+      end
+    end
+
+
   end
 
 end
