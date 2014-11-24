03d526f29ec8651f9d79dcc9a08d2c857b41837a
George Millo <georgejulianmillo@gmail.com>
2014-11-24 12:57:02 +0700

    removing deprecation warnings from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path("../../spec/dummy/config/environment", __FILE__)
 
 require 'database_cleaner'
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -21,5 +20,4 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
