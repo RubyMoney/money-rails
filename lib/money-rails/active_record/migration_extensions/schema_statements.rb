bcb74f63d144624e5dd1fde9df7faa32e90a054c
Mohamed Wael Khobalatte <wael.khobalatte@gmail.com>
2014-12-02 19:50:11 -0500

    Deprecate money migration helpers
    These helpers conflicted with a new money type that was added
    to rails-4.2.beta4.

--- lib/money-rails/active_record/migration_extensions/schema_statements.rb
+++ lib/money-rails/active_record/migration_extensions/schema_statements.rb
@@ -3,6 +3,14 @@ module MoneyRails
     module MigrationExtensions
       module SchemaStatements
         def add_money(table_name, accessor, options={})
+          MoneyRails.deprecator.warn(<<-MSG.squish)
+            `#t.add_money` is deprecated and will be removed in money-rails version 2. Either
+            explicity name your money and currency columns `COLUMN_NAME_cents` and
+            `COLUMN_NAME_currency` or pick a different name. If you pick the latter
+            option you will need to instruct your model to monitize the correct columns.
+            See the README for details.
+          MSG
+
           [:amount, :currency].each do |attribute|
             column_present, *opts = OptionsExtractor.extract attribute, table_name, accessor, options
             add_column *opts if column_present
@@ -10,6 +18,14 @@ module MoneyRails
         end
 
         def remove_money(table_name, accessor, options={})
+          MoneyRails.deprecator.warn(<<-MSG.squish)
+            `#t.remove_money` is deprecated and will be removed in money-rails version 2. Either
+            explicity name your money and currency columns `COLUMN_NAME_cents` and
+            `COLUMN_NAME_currency` or pick a different name. If you pick the latter
+            option you will need to instruct your model to monitize the correct columns.
+            See the README for details.
+          MSG
+
           [:amount, :currency].each do |attribute|
             column_present, table_name, column_name, _, _ =  OptionsExtractor.extract attribute, table_name, accessor, options
             remove_column table_name, column_name if column_present
