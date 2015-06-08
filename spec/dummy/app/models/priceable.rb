892006a8b204d25132fab8d67751328c4d204097
Sai <github@saizai.com>
2015-06-08 12:35:46 +0100

    rv allow_nil (not allowed in mongoid)

--- spec/dummy/app/models/priceable.rb
+++ spec/dummy/app/models/priceable.rb
@@ -2,7 +2,7 @@ if defined? Mongoid
   class Priceable
     include Mongoid::Document
 
-    field :price, :type => Money, :allow_nil => true
+    field :price, :type => Money
     field :price_hash, :type => Hash
   end
 end
