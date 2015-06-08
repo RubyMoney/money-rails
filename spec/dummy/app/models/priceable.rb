af87ee0713cf178465b500ddce48a71089b96354
Sai <github@saizai.com>
2015-06-08 12:23:15 +0100

    Allow nil, per specs' expectation

--- spec/dummy/app/models/priceable.rb
+++ spec/dummy/app/models/priceable.rb
@@ -2,7 +2,7 @@ if defined? Mongoid
   class Priceable
     include Mongoid::Document
 
-    field :price, :type => Money
+    field :price, :type => Money, :allow_nil => true
     field :price_hash, :type => Hash
   end
 end
