5705fe96cff432cb401ab3ac0bf648056b94c015
Mike Virata-Stone <mike@virata-stone.com>
2015-09-20 23:14:25 -0700

    Database transactions around the specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,28 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 ENV["RACK_ENV"] = "test"
 require "gemstash"
 require "dalli"
+require "fileutils"
 require "support/matchers"
 
+TEST_BASE_PATH = File.expand_path("../../tmp/test_base", __FILE__)
+FileUtils.mkpath(TEST_BASE_PATH) unless Dir.exist?(TEST_BASE_PATH)
+TEST_CONFIG = Gemstash::Env::DEFAULT_CONFIG.merge(
+  :base_path => TEST_BASE_PATH
+).freeze
+
 RSpec.configure do |config|
+  config.around(:each) do |example|
+    unless Gemstash::Env.config == TEST_CONFIG
+      Gemstash::Env.config = TEST_CONFIG
+    end
+
+    Gemstash::Env.db.transaction(:rollback => :always) do
+      example.run
+    end
+
+    Gemstash::Env.db.disconnect
+  end
+
   config.before(:each) do
     Gemstash::Env.cache_client.flush
   end
