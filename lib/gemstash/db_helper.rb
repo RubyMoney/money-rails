adc9961ff42168216924acc265423e335717402e
Mike Virata-Stone <mjstone@on-site.com>
2015-09-21 17:13:06 -0700

    Push database logic into Gemstash::DBHelper

--- lib/gemstash/db_helper.rb
+++ lib/gemstash/db_helper.rb
@@ -1,13 +1,44 @@
 module Gemstash
   #:nodoc:
   class DBHelper
-    def find_or_insert_rubygem(name)
-      row = Gemstash::Env.db[:rubygems][:name => name]
+    def find_or_insert_rubygem(spec)
+      row = Gemstash::Env.db[:rubygems][:name => spec.name]
       return row[:id] if row
       Gemstash::Env.db[:rubygems].insert(
-        :name => name,
+        :name => spec.name,
         :created_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP,
         :updated_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP)
+    end
+
+    def find_version(gem_id, spec)
+      Gemstash::Env.db[:versions][
+        :rubygem_id => gem_id,
+        :number => spec.version.to_s,
+        :platform => spec.platform]
+    end
+
+    def insert_version(gem_id, spec, indexed = true)
+      Gemstash::Env.db[:versions].insert(
+        :rubygem_id => gem_id,
+        :number => spec.version.to_s,
+        :platform => spec.platform,
+        :indexed => indexed,
+        :created_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP,
+        :updated_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP)
+    end
+
+    def insert_dependencies(version_id, spec)
+      spec.runtime_dependencies.each do |dep|
+        requirements = dep.requirement.requirements
+        requirements = requirements.map {|r| "#{r.first} #{r.last}" }
+        requirements = requirements.join(", ")
+        Gemstash::Env.db[:dependencies].insert(
+          :version_id => version_id,
+          :rubygem_name => dep.name,
+          :requirements => requirements,
+          :created_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP,
+          :updated_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP)
+      end
     end
   end
 end
--- lib/gemstash/gem_pusher.rb
+++ lib/gemstash/gem_pusher.rb
@@ -31,12 +31,8 @@ module Gemstash
       spec = gem.spec
 
       Gemstash::Env.db.transaction do
-        gem_id = @db_helper.find_or_insert_rubygem(spec.name)
-
-        existing = Gemstash::Env.db[:versions][
-          :rubygem_id => gem_id,
-          :number => spec.version.to_s,
-          :platform => spec.platform]
+        gem_id = @db_helper.find_or_insert_rubygem(spec)
+        existing = @db_helper.find_version(gem_id, spec)
 
         if existing
           if existing[:indexed]
@@ -46,25 +42,8 @@ module Gemstash
           end
         end
 
-        version_id = Gemstash::Env.db[:versions].insert(
-          :rubygem_id => gem_id,
-          :number => spec.version.to_s,
-          :platform => spec.platform,
-          :indexed => true,
-          :created_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP,
-          :updated_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP)
-
-        spec.runtime_dependencies.each do |dep|
-          requirements = dep.requirement.requirements
-          requirements = requirements.map {|r| "#{r.first} #{r.last}" }
-          requirements = requirements.join(", ")
-          Gemstash::Env.db[:dependencies].insert(
-            :version_id => version_id,
-            :rubygem_name => dep.name,
-            :requirements => requirements,
-            :created_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP,
-            :updated_at => Sequel::SQL::Constants::CURRENT_TIMESTAMP)
-        end
+        version_id = @db_helper.insert_version(gem_id, spec)
+        @db_helper.insert_dependencies(version_id, spec)
       end
     end
   end
