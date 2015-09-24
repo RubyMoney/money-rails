7fb92c6932d22dba99dbd8bf1fcfa5aa5eda9b6b
Mike Virata-Stone <mjstone@on-site.com>
2015-09-23 20:11:03 -0700

    Stubs for new integration specs that are coming

---
+++ spec/integration_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe "bundle install against gemstash" do
+  context "with just cached gems" do
+    it "successfully bundles"
+  end
+
+  context "with just private gems" do
+    it "successfully bundles"
+  end
+
+  context "with private and cached gems" do
+    it "successfully bundles"
+  end
+
+  context "with private versions overriding public gems" do
+    it "successfully bundles"
+  end
+end
