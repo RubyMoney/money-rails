fdfddd4511002adeaec2a3b50f9069383b69c8e4
Pablo Carranza <pcarranza@gmail.com>
2015-10-08 18:10:07 -0400

    Use to_s instead of url when building the http client to make it language compatible

--- lib/gemstash/http_client.rb
+++ lib/gemstash/http_client.rb
@@ -18,7 +18,7 @@ module Gemstash
     DEFAULT_USER_AGENT = "Gemstash #{Gemstash::VERSION}"
 
     def self.for(upstream)
-      client = Faraday.new(upstream.url) do |config|
+      client = Faraday.new(upstream.to_s) do |config|
         config.use FaradayMiddleware::FollowRedirects
         config.adapter :net_http
       end
