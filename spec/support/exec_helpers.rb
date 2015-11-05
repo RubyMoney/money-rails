b67653863fec59beea9771907e69c26ebf7cdeb7
Mike Virata-Stone <mjstone@on-site.com>
2015-11-04 16:42:06 -0800

    Break up massive execute method into smaller chunks inside the Result class

--- spec/support/exec_helpers.rb
+++ spec/support/exec_helpers.rb
@@ -4,31 +4,6 @@ require "open3"
 # Helpers for executing commands and asserting the results.
 module ExecHelpers
   def execute(command, dir: nil, env: {})
-    bundle_gemfile = nil
-
-    if dir
-      gemfile_path = File.join(dir, "Gemfile")
-      bundle_gemfile = gemfile_path if File.exist?(gemfile_path)
-    end
-
-    default_env = {
-      "BUNDLE_GEMFILE" => bundle_gemfile,
-      "RUBYLIB" => nil,
-      "RUBYOPT" => nil,
-      "GEM_PATH" => ENV["_ORIGINAL_GEM_PATH"]
-    }
-
-    if RUBY_PLATFORM == "java"
-      default_env["JRUBY_OPTS"] = "--dev"
-
-      if command.start_with?("bundle")
-        bundler_patch = File.expand_path("../jruby_bundler_monkeypatch.rb", __FILE__)
-        default_env["JRUBY_OPTS"] += " -r#{bundler_patch}"
-      end
-    end
-
-    env = default_env.merge(env)
-    dir ||= File.expand_path(".")
     Result.new(env, command, dir)
   end
 
@@ -38,8 +13,10 @@ module ExecHelpers
 
     def initialize(env, command, dir)
       @command = command
-      @dir = dir
-      @output, @status = Open3.capture2e(env, command, chdir: dir)
+      @env = env
+      @original_dir = dir
+      @dir = dir || File.expand_path(".")
+      @output, @status = Open3.capture2e(patched_env, @command, chdir: @dir)
       fix_jruby_output
     end
 
@@ -64,6 +41,40 @@ module ExecHelpers
       end
 
       @output.gsub!(/^.*warning: unsupported exec option: close_others\n/, "")
+    end
+
+    def patched_env
+      @patched_env ||= faster_jruby_env.merge(clear_bundler_env).merge(clear_ruby_env).merge(@env)
+    end
+
+    def clear_ruby_env
+      {
+        "RUBYLIB" => nil,
+        "RUBYOPT" => nil,
+        "GEM_PATH" => ENV["_ORIGINAL_GEM_PATH"]
+      }
+    end
+
+    def clear_bundler_env
+      if @original_dir
+        gemfile_path = File.join(@original_dir, "Gemfile")
+        bundle_gemfile = gemfile_path if File.exist?(gemfile_path)
+      end
+
+      { "BUNDLE_GEMFILE" => bundle_gemfile }
+    end
+
+    def faster_jruby_env
+      return {} unless RUBY_PLATFORM == "java"
+      jruby_opts = "--dev"
+
+      if command.start_with?("bundle")
+        bundler_patch = File.expand_path("../jruby_bundler_monkeypatch.rb", __FILE__)
+        raise "Spaces in your path will cause build failures!" if bundler_patch.include?(" ")
+        jruby_opts += " -r#{bundler_patch}"
+      end
+
+      { "JRUBY_OPTS" => jruby_opts }
     end
   end
 end
