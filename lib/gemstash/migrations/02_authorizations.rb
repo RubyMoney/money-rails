efe2e9687842336ae5188238545c9238629f84da
Pablo Carranza <pcarranza@gmail.com>
2015-11-29 18:03:34 +0000

    Reduce length of authorizations so mysql does not complain of it being too large

--- lib/gemstash/migrations/02_authorizations.rb
+++ lib/gemstash/migrations/02_authorizations.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   change do
     create_table :authorizations do
       primary_key :id
-      String :auth_key, :size => 2056, :null => false
+      String :auth_key, :size => 255, :null => false
       String :permissions, :size => 255, :null => false
       DateTime :created_at, :null => false
       DateTime :updated_at, :null => false
