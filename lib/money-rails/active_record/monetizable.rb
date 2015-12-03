0a67135202d6d332bd902c143d97fbe48eeb513c
GCorbel <guirec.corbel@gmail.com>
2015-12-03 08:32:06 -0500

    Create an attribute writer

--- lib/money-rails/active_record/monetizable.rb
+++ lib/money-rails/active_record/monetizable.rb
@@ -194,23 +194,75 @@ module MoneyRails
           end
         end
 
-        def define_writer_method(attribute)
-          name = attribute.name
-          instance_currency_name = attribute.instance_currency_name
-          define_method "#{name}=" do |value|
+        class AttributeWriter
+          attr_accessor :model, :name, :instance_currency_name, :value,
+            :options, :attribute
 
+          def initialize(model, attribute, value)
+            @model = model
+            @name = attribute.name
+            @instance_currency_name = attribute.instance_currency_name
+            @options = attribute.options
+            @value = value
+            @attribute = attribute
+          end
+
+          def call
             # Lets keep the before_type_cast value
-            instance_variable_set "@#{name}_money_before_type_cast", value
+            model.instance_variable_set "@#{name}_money_before_type_cast", value
+            update_cents
+            update_currency
 
+            # Save and return the new Money object
+            model.instance_variable_set "@#{name}", money
+          end
+
+          private
+
+          def update_currency
+            money_currency = money.try(:currency)
+            # Update currency iso value if there is an instance currency attribute
+            if instance_currency_name.present? && model.respond_to?("#{instance_currency_name}=") && money_currency
+              model.public_send("#{instance_currency_name}=", money_currency.try(:iso_code))
+            else
+              current_currency = model.public_send("currency_for_#{name}")
+              if money_currency && current_currency != money_currency.id
+                raise ReadOnlyCurrencyException.new("Can't change readonly currency '#{current_currency}' to '#{money_currency}' for field '#{name}'") if MoneyRails.raise_error_on_money_parsing
+                return nil
+              end
+            end
+          end
+
+          def update_cents
+            cents = money.try(:cents)
+
+            if !attribute.validation_enabled?
+              # We haven't defined our own subunit writer, so we can invoke
+              # the regular writer, which works with store_accessors
+              model.public_send("#{attribute.subunit_name}=", cents)
+            elsif model.class.respond_to?(:attribute_aliases) &&
+              model.class.attribute_aliases.key?(attribute.subunit_name)
+              # If the attribute is aliased, make sure we write to the original
+              # attribute name or an error will be raised.
+              # (Note: 'attribute_aliases' doesn't exist in Rails 3.x, so we
+              # can't tell if the attribute was aliased.)
+              original_name = model.class.attribute_aliases[attribute.subunit_name.to_s]
+              model.send(:write_attribute, original_name, cents)
+            else
+              model.send(:write_attribute, attribute.subunit_name, cents)
+            end
+          end
+
+          def money
             # Use nil or get a Money object
-            if attribute.options[:allow_nil] && value.blank?
+            if options[:allow_nil] && value.blank?
               money = nil
             else
               if value.is_a?(Money)
                 money = value
               else
                 begin
-                  money = value.to_money(public_send("currency_for_#{name}"))
+                  money = value.to_money(model.public_send("currency_for_#{name}"))
                 rescue NoMethodError
                   return nil
                 rescue ArgumentError
@@ -222,39 +274,13 @@ module MoneyRails
                 end
               end
             end
+          end
+        end
 
-            # Update cents
-            if !attribute.validation_enabled?
-              # We haven't defined our own subunit writer, so we can invoke
-              # the regular writer, which works with store_accessors
-              public_send("#{attribute.subunit_name}=", money.try(:cents))
-            elsif self.class.respond_to?(:attribute_aliases) &&
-              self.class.attribute_aliases.key?(attribute.subunit_name)
-              # If the attribute is aliased, make sure we write to the original
-              # attribute name or an error will be raised.
-              # (Note: 'attribute_aliases' doesn't exist in Rails 3.x, so we
-              # can't tell if the attribute was aliased.)
-              original_name = self.class.attribute_aliases[attribute.subunit_name.to_s]
-              write_attribute(original_name, money.try(:cents))
-            else
-              write_attribute(attribute.subunit_name, money.try(:cents))
-            end
-
-            money_currency = money.try(:currency)
-
-            # Update currency iso value if there is an instance currency attribute
-            if instance_currency_name.present? && respond_to?("#{instance_currency_name}=") && money_currency
-              public_send("#{instance_currency_name}=", money_currency.try(:iso_code))
-            else
-              current_currency = public_send("currency_for_#{name}")
-              if money_currency && current_currency != money_currency.id
-                raise ReadOnlyCurrencyException.new("Can't change readonly currency '#{current_currency}' to '#{money_currency}' for field '#{name}'") if MoneyRails.raise_error_on_money_parsing
-                return nil
-              end
-            end
-
-            # Save and return the new Money object
-            instance_variable_set "@#{name}", money
+        def define_writer_method(attribute)
+          name = attribute.name
+          define_method "#{name}=" do |value|
+            AttributeWriter.new(self, attribute, value).call
           end
         end
 
