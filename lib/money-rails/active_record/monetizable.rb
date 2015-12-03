113960a6b9982879e81b5b0c8165bb2e960a467f
GCorbel <guirec.corbel@gmail.com>
2015-12-03 07:51:04 -0500

    Create a currency finder

--- lib/money-rails/active_record/monetizable.rb
+++ lib/money-rails/active_record/monetizable.rb
@@ -121,34 +121,67 @@ module MoneyRails
           end
         end
 
-        def defined_currency_for_method(attribute)
-          name = attribute.name
-          instance_currency_name = attribute.instance_currency_name
-          field_currency_name = attribute.field_currency_name
+        class CurrencyFinder
+          attr_accessor :model, :name, :instance_currency_name,
+            :field_currency_name, :instance_currency_name_with_postfix
 
-          define_method "currency_for_#{name}" do
+          def initialize(model, attribute)
+            @model = model
+            @name = attribute.name
+            @instance_currency_name = attribute.instance_currency_name
+            @field_currency_name = attribute.field_currency_name
             if MoneyRails::Configuration.currency_column[:postfix].present?
-              instance_currency_name_with_postfix = "#{name}#{MoneyRails::Configuration.currency_column[:postfix]}"
+              @instance_currency_name_with_postfix = "#{name}#{MoneyRails::Configuration.currency_column[:postfix]}"
             end
+          end
 
-            if instance_currency_name.present? && respond_to?(instance_currency_name) &&
-                Money::Currency.find(public_send(instance_currency_name))
+          def call
+            instance_currency || field_currency ||
+              instance_currency_with_postfix ||
+              class_currency || default_currency
+          end
 
-              Money::Currency.find(public_send(instance_currency_name))
-            elsif field_currency_name.respond_to?(:call)
-              Money::Currency.find(field_currency_name.call(self))
+          private
+
+          def default_currency
+            Money.default_currency
+          end
+
+          def class_currency
+            if model.class.respond_to?(:currency)
+              model.class.currency
+            end
+          end
+
+          def instance_currency_with_postfix
+            if instance_currency_name_with_postfix.present? &&
+                model.respond_to?(instance_currency_name_with_postfix) &&
+                Money::Currency.find(model.public_send(instance_currency_name_with_postfix))
+
+              Money::Currency.find(model.public_send(instance_currency_name_with_postfix))
+            end
+          end
+
+          def field_currency
+            if field_currency_name.respond_to?(:call)
+              Money::Currency.find(field_currency_name.call(model))
             elsif field_currency_name
               Money::Currency.find(field_currency_name)
-            elsif instance_currency_name_with_postfix.present? &&
-              respond_to?(instance_currency_name_with_postfix) &&
-              Money::Currency.find(public_send(instance_currency_name_with_postfix))
-
-              Money::Currency.find(public_send(instance_currency_name_with_postfix))
-            elsif self.class.respond_to?(:currency)
-              self.class.currency
-            else
-              Money.default_currency
             end
+          end
+
+          def instance_currency
+            if instance_currency_name.present? && model.respond_to?(instance_currency_name) &&
+                Money::Currency.find(model.public_send(instance_currency_name))
+
+              Money::Currency.find(model.public_send(instance_currency_name))
+            end
+          end
+        end
+
+        def defined_currency_for_method(attribute)
+          define_method "currency_for_#{attribute.name}" do
+            CurrencyFinder.new(self, attribute).call
           end
         end
 
