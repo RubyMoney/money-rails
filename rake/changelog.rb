68ac9dfdb70fc40e9057c0a221bd2092dbf1f407
Mike Virata-Stone <mjstone@on-site.com>
2015-12-08 15:33:04 -0800

    Lay out methods for the structure of what Changelog.run will do Check that the version has been updated

--- rake/changelog.rb
+++ rake/changelog.rb
@@ -1,5 +1,36 @@
 # Helper class for updating CHANGELOG.md
 class Changelog
+  attr_reader :changelog
+
+  def initialize
+    @changelog = File.expand_path("../../CHANGELOG.md", __FILE__)
+  end
+
   def run
+    ensure_new_version_specified
+    parse_current_version
+    return unless missing_pull_requests?
+    fetch_missing_pull_requests
+    update_changelog
+  end
+
+  def ensure_new_version_specified
+    require_relative "../lib/gemstash/version.rb"
+    tags = `git tag -l`
+    return unless tags.include? Gemstash::VERSION
+    STDERR.puts "Please update lib/gemstash/version.rb with the new version first!"
+    exit false
+  end
+
+  def parse_current_version
+  end
+
+  def missing_pull_requests?
+  end
+
+  def fetch_missing_pull_requests
+  end
+
+  def update_changelog
   end
 end
