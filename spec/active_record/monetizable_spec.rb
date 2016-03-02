9afd399164d5b7286065ae6ab80e71b6f0644c3c
Anthony Dmitriyev <antstorm@gmail.com>
2016-03-02 17:48:56 +0000

    Extract write_monetized method outside of monetize

--- spec/active_record/monetizable_spec.rb
+++ spec/active_record/monetizable_spec.rb
@@ -844,6 +844,129 @@ if defined? ActiveRecord
       end
     end
 
+    describe "#write_monetized" do
+      let(:value) { Money.new(1_000, 'LVL') }
+
+      it "sets monetized attribute's value to Money object" do
+        product.write_monetized :price, :price_cents, value, false, nil, {}
+
+        expect(product.price).to be_an_instance_of(Money)
+        expect(product.price_cents).to eq(value.cents)
+        # Because :price does not have a column for currency
+        expect(product.price.currency).to eq(Product.currency)
+      end
+
+      it "sets monetized attribute's value from a given Fixnum" do
+        product.write_monetized :price, :price_cents, 10, false, nil, {}
+
+        expect(product.price).to be_an_instance_of(Money)
+        expect(product.price_cents).to eq(1000)
+      end
+
+      it "sets monetized attribute's value from a given Float" do
+        product.write_monetized :price, :price_cents, 10.5, false, nil, {}
+
+        expect(product.price).to be_an_instance_of(Money)
+        expect(product.price_cents).to eq(1050)
+      end
+
+      it "resets monetized attribute when given blank input" do
+        product.write_monetized :price, :price_cents, nil, false, nil, { :allow_nil => true }
+
+        expect(product.price).to eq(nil)
+      end
+
+      it "sets monetized attribute to 0 when given a blank value" do
+        currency = product.price.currency
+        product.write_monetized :price, :price_cents, nil, false, nil, {}
+
+        expect(product.price.amount).to eq(0)
+        expect(product.price.currency).to eq(currency)
+      end
+
+      it "does not memoize monetized attribute's value if currency is read-only" do
+        product.write_monetized :price, :price_cents, value, false, nil, {}
+
+        price = product.instance_variable_get('@price')
+
+        expect(price).to be_an_instance_of(Money)
+        expect(price.amount).not_to eq(value.amount)
+      end
+
+      describe "instance_currency_name" do
+        it "updates instance_currency_name attribute" do
+          product.write_monetized :sale_price, :sale_price_amount, value, false, :sale_price_currency_code, {}
+
+          expect(product.sale_price).to eq(value)
+          expect(product.sale_price_currency_code).to eq('LVL')
+        end
+
+        it "memoizes monetized attribute's value with currency" do
+          product.write_monetized :sale_price, :sale_price_amount, value, false, :sale_price_currency_code, {}
+
+          expect(product.instance_variable_get('@sale_price')).to eq(value)
+        end
+
+        it "ignores empty instance_currency_name" do
+          product.write_monetized :sale_price, :sale_price_amount, value, false, '', {}
+
+          expect(product.sale_price.amount).to eq(value.amount)
+          expect(product.sale_price.currency).to eq(Product.currency)
+        end
+
+        it "ignores instance_currency_name that model does not respond to" do
+          product.write_monetized :sale_price, :sale_price_amount, value, false, :non_existing_currency, {}
+
+          expect(product.sale_price.amount).to eq(value.amount)
+          expect(product.sale_price.currency).to eq(Product.currency)
+        end
+      end
+
+      describe "error handling" do
+        let!(:old_price_value) { product.price }
+
+        it "ignores values that do not implement to_money method" do
+          product.write_monetized :price, :price_cents, [10], false, nil, {}
+
+          expect(product.price).to eq(old_price_value)
+        end
+
+        context "raise_error_on_money_parsing enabled" do
+          before { MoneyRails.raise_error_on_money_parsing = true }
+          after { MoneyRails.raise_error_on_money_parsing = false }
+
+          it "raises an ArgumentError when given an invalid value" do
+            expect {
+              product.write_monetized :price, :price_cents, '10-50', false, nil, {}
+            }.to raise_error(ArgumentError)
+          end
+
+          it "raises a Money::Currency::UnknownCurrency error when trying to set invalid currency" do
+            allow(product).to receive(:currency_for_price).and_return('INVALID_CURRENCY')
+            expect {
+              product.write_monetized :price, :price_cents, 10, false, nil, {}
+            }.to raise_error(Money::Currency::UnknownCurrency)
+          end
+        end
+
+        context "raise_error_on_money_parsing disabled" do
+          it "ignores when given invalid value" do
+            product.write_monetized :price, :price_cents, '10-50', false, nil, {}
+
+            expect(product.price).to eq(old_price_value)
+          end
+
+          it "raises a Money::Currency::UnknownCurrency error when trying to set invalid currency" do
+            allow(product).to receive(:currency_for_price).and_return('INVALID_CURRENCY')
+            product.write_monetized :price, :price_cents, 10, false, nil, {}
+
+            # Can not use public accessor here because currency_for_price is stubbed
+            expect(product.instance_variable_get('@price')).to eq(old_price_value)
+          end
+        end
+      end
+    end
+
     describe "#currency_for" do
       it "detects currency based on instance currency name" do
         product = Product.new(:sale_price_currency_code => 'CAD')
