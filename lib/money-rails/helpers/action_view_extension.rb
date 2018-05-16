11665f7c6361796f558fbc109f6881b8c8c078fd
Rickard Sunden <rickard.sunden@outlook.com>
2018-05-15 20:47:50 -0400

    Add spacing back for readability.

--- lib/money-rails/helpers/action_view_extension.rb
+++ lib/money-rails/helpers/action_view_extension.rb
@@ -10,11 +10,13 @@ module MoneyRails
         warn "humanized_money now takes a hash of formatting options, please specify { symbol: true }"
         options = { symbol: options }
       end
+
       options = {
         no_cents_if_whole: MoneyRails::Configuration.no_cents_if_whole == false ? false : true,
         symbol: false
       }.merge(options)
       options.delete(:symbol) if options[:disambiguate]
+
       if value.is_a?(Money)
         value.format(options)
       elsif value.respond_to?(:to_money)
