37fd8afa69301b18fa17343541efa3d67a1f1caa
Andreas Eger <dev@eger-andreas.de>
2018-06-04 12:55:52 +0200

    fix rubocop

--- lib/gemstash/puma.rb
+++ lib/gemstash/puma.rb
@@ -2,5 +2,5 @@ require "gemstash"
 
 threads 0, Gemstash::Env.current.config[:puma_threads].to_i
 bind Gemstash::Env.current.config[:bind].to_s
-workers Gemstash::Env.current.config[:puma_workers].to_i unless RUBY_PLATFORM == 'java'
+workers Gemstash::Env.current.config[:puma_workers].to_i unless RUBY_PLATFORM == "java"
 rackup Gemstash::Env.current.rackup
