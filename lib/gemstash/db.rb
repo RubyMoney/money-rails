7f2ec53f26e5830925c09099a7c8071626fea3f1
bronzdoc <lsagastume1990@gmail.com>
2018-11-21 20:17:55 -0600

    Fix Layout/EmptyLineAfterGuardClause

--- lib/gemstash/db.rb
+++ lib/gemstash/db.rb
@@ -6,6 +6,7 @@ module Gemstash
   # Module containing the DB models.
   module DB
     raise "Gemstash::DB cannot be loaded until the Gemstash::Env is available" unless Gemstash::Env.available?
+
     Sequel::Model.db = Gemstash::Env.current.db
     Sequel::Model.raise_on_save_failure = true
     Sequel::Model.plugin :timestamps, update_on_create: true
