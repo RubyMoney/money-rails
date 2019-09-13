b60ad7d00ce563b4104a74e7f878cb6ae259e101
Ingo Tomahogh-Willms <Contact@Itowi.de>
2019-09-13 17:31:27 +0200

    Update schema_statements_pg_rails4.rb
    Making `remove_monetize` / `remove_money` reversible (which is useful with `rails db:rollback`).
    
    ("remove_column is only reversible if given a type.")

--- lib/money-rails/active_record/migration_extensions/schema_statements_pg_rails4.rb
+++ lib/money-rails/active_record/migration_extensions/schema_statements_pg_rails4.rb
@@ -11,8 +11,8 @@ module MoneyRails
 
         def remove_monetize(table_name, accessor, options={})
           [:amount, :currency].each do |attribute|
-            column_present, table_name, column_name, _, _ =  OptionsExtractor.extract attribute, table_name, accessor, options
-            remove_column table_name, column_name if column_present
+            column_present, table_name, column_name, type, _ =  OptionsExtractor.extract attribute, table_name, accessor, options
+            remove_column table_name, column_name, type if column_present
           end
         end
       end
