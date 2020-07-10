9fbf693e29423a8efb4b2bef6938aaba074d4a5e
Jared Szechy <jared.szechy@gmail.com>
2020-07-09 20:55:05 -0400

    remove unneeded comma

--- lib/money-rails/active_record/migration_extensions/schema_statements_pg_rails4.rb
+++ lib/money-rails/active_record/migration_extensions/schema_statements_pg_rails4.rb
@@ -11,7 +11,7 @@ module MoneyRails
 
         def remove_monetize(table_name, accessor, options = {})
           [:amount, :currency].each do |attribute|
-            column_present, table_name, column_name, type, = OptionsExtractor.extract attribute, table_name, accessor, options
+            column_present, table_name, column_name, type = OptionsExtractor.extract attribute, table_name, accessor, options
             remove_column table_name, column_name, type if column_present
           end
         end
