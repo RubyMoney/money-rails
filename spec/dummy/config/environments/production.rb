7570b22e90314bdef606264d8bb8f7d7c49d3795
Sunny Ripert <sunny@sunfox.org>
2026-01-13 16:06:12 +0100

    Replace http links in favor of https in documentation (#732)

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -49,7 +49,7 @@ Dummy::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # config.action_controller.asset_host = "https://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
