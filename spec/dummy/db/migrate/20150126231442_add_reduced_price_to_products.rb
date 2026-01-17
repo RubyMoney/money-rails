3cba4b0007478bafcc654e4a34de78b286392703
Christian Schmidt <cs@relatel.dk>
2026-01-17 11:10:11 +0100

    Do not run migrations in tests

--- spec/dummy/db/migrate/20150126231442_add_reduced_price_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddReducedPriceToProducts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :products, :reduced_price_cents, :integer
-    add_column :products, :reduced_price_currency, :string
-  end
-end
